230057f5df8469bf5e1301c45c0f18b3061f6227
Şehmus Emre DEMİRALAY <sehmusemre.demiralay@tuikmerkez.tuik.gov.tr>
2022-12-26 12:07:56 +0300

    first load

---
+++ src/main/java/com/emre/spring/data/jpa/tutorial/repository/CourseMaterialRepository.java
@@ -0,0 +1,9 @@
+package com.emre.spring.data.jpa.tutorial.repository;
+
+import com.emre.spring.data.jpa.tutorial.entity.CourseMaterial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseMaterialRepository extends JpaRepository<CourseMaterial,Long> {
+}
---
+++ src/test/java/com/emre/spring/data/jpa/tutorial/repository/StudentRepositoryTest.java
@@ -0,0 +1,131 @@
+package com.emre.spring.data.jpa.tutorial.repository;
+
+import com.emre.spring.data.jpa.tutorial.entity.Guardian;
+import com.emre.spring.data.jpa.tutorial.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent() {
+        Student student = Student.builder()
+                .emailId("eeee")
+                .firstName("ffffffff")
+                .lastName("bbbbbbbbb")
+                //.guardianName("Nikhil")
+                //.guardianEmail("nikhil@gmail.com")
+                //.guardianMobile("9999999999")
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void saveStudentWithGuardian() {
+
+        Guardian guardian = Guardian.builder()
+                .email("1nidkhil@gmail.com")
+                .name("dd")
+                .mobile("9999956324")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("ff")
+                .emailId("e1@gmail.com")
+                .lastName("gggg")
+                .guardian(guardian)
+                .build();
+
+        studentRepository.save(student);
+
+    }
+    
+    @Test
+    public void printAllStudent() {
+        List<Student> studentList =
+                studentRepository.findAll();
+
+        System.out.println("studentList = " + studentList);
+    }
+    
+    @Test
+    public void printStudentByFirstName() {
+        
+        List<Student> students =
+                studentRepository.findByFirstName("xx");
+
+        System.out.println("students = " + students);
+    }
+
+    @Test
+    public void printStudentByFirstNameContaining() {
+
+        List<Student> students =
+                studentRepository.findByFirstNameContaining("sh");
+
+        System.out.println("students = " + students);
+
+    }
+    
+    @Test
+    public void printStudentBasedOnGuardianName(){
+        List<Student> students =
+                studentRepository.findByGuardianName("Nikhil");
+        System.out.println("students = " + students);
+    }
+
+    @Test
+    public void printgetStudentByEmailAddress() {
+        Student student =
+                studentRepository.getStudentByEmailAddress(
+                        "shabbir@gmail.com"
+                );
+
+        System.out.println("student = " + student);
+    }
+
+    @Test
+    public void printgetStudentFirstNameByEmailAddress() {
+        String firstName =
+                studentRepository.getStudentFirstNameByEmailAddress(
+                        "dd@gmail.com"
+                );
+        System.out.println("firstName = " + firstName);
+    }
+    
+    @Test
+    public void printgetStudentByEmailAddressNative(){
+        Student student =
+                studentRepository.getStudentByEmailAddressNative(
+                        "dd@gmail.com"
+                );
+
+        System.out.println("student = " + student);
+    }
+
+    @Test
+    public void printgetStudentByEmailAddressNativeNamedParam() {
+        Student student =
+                studentRepository.getStudentByEmailAddressNativeNamedParam(
+                        "dd@gmail.com"
+                );
+
+        System.out.println("student = " + student);
+    }
+
+    @Test
+    public void updateStudentNameByEmailIdTest() {
+        studentRepository.updateStudentNameByEmailId(
+                "muhammed",
+                "dd@gmail.com");
+    }
+
+}
